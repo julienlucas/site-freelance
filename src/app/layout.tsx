305b5678f0cab70561142adb80d261f9b902f8a1
julienlucas <julienlucas-ext@smartch.fr>
2024-05-26 14:34:20 +0200

    Change GTM id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Manrope, Unbounded } from "next/font/google";
 import "./globals.css";
 
@@ -17,7 +18,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${unbounded.variable} ${manrope.variable}`}>{children}</body>
+      <body className={`${unbounded.variable} ${manrope.variable}`}>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-Y3PM4DBLGR`}
+          strategy="afterInteractive"
+        />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Y3PM4DBLGR', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+          strategy="afterInteractive"
+        />
+        {children}
+      </body>
     </html>
   );
 }
